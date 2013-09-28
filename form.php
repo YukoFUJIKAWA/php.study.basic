7e31e30bcf6f0c1736191f675b141d29a9f1a0b9
mon_sat <tanaka.kohji+github@gmail.com>
2013-09-28 11:14:35 +0900

    received POST

--- form.php
+++ form.php
@@ -1,3 +1,22 @@
 <?php
 
+// POSTされた内容を表示する
+echo '<b>$_POST</b>:';
+var_dump($_POST);
+echo '<br>';
+
+if (isset($_POST{'title'})) {
+	echo '<b>$_POST["title"]</b>:';
+	var_dump($_POST['title']);
+	echo '<br>';
+}
+
+if (isset($_POST{'body'})) {
+	echo '<b>$_POST["body"]</b>:';
+	var_dump($_POST['body']);
+	echo '<br>';
+}
+echo '<hr>';
+
+// 送信完了
 echo '送信しました';
